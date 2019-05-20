49066424cd4b82e7729c20b83dfc6e7fc76e52ce
humingdong <18360731997@163.com>
2019-05-20 08:21:55 +0800

    后台接口

--- src/components/bignumbers/bignumbers.vue
+++ src/components/bignumbers/bignumbers.vue
@@ -17,20 +17,62 @@ export default {
         output:Number, //产量
         inventory:Number, //库存
         sales:Number,//销售量
+        yesterdayoutput:String, //昨日产量
+        sevendaysoutput:String, //七日产量
+        monthoutput:String, //月产量
+        yesterdayinventory:String, //昨日库存
+        sevendayinventory:Number, //七日库存
+        monthinventory:String, //月库存
+        yesterdaysales:String, //昨日销量
+        sevendayssales:String, //    七日销量
+        monthsales:String //月销量
       }
   },
-  created(){
-      if(this.code === "yesterday"){
-          this.output = 2789;
-          this.inventory = 3131;
-          this.sales = 2542
-       }
+  mouted(){
 
-       if(this.code === "sevendays"){
-          this.output = 3279;
-          this.inventory = 5555;
-          this.sales = 5902
+  },
+  created(){
+      let me  = this
+    var day1 = new Date();
+     day1.setTime(day1.getTime()-24*60*60*1000);
+     var s1 = day1.getFullYear()+"-0" + (day1.getMonth()+1) + "-" + day1.getDate();
+      this.$http.get('http://61.177.139.229:8083/Liems/webservice/getLateProductList?backdate='+"2019-05-06").then((response) => {
+         console.log(response.data["当日产量"])
+         if(this.code === "yesterday"){
+          this.output = response.data["当日产量"][0].dr_zcl;
        }
+      });
+      
+      this.$http.get('http://61.177.139.229:8083/Liems/webservice/getLateProductList').then((response) => {
+       me.sevendaysoutput = response.data["7日合计"]
+       if(this.code === "sevendays"){
+           console.log(me.sevendaysoutput)
+          this.output = me.sevendaysoutput;
+       }
+      });
+
+    //   this.$http.get('http://61.177.139.229:8083/Liems/webservice/getLateStockList?backdate=2019-05-06').then((response) => {
+    // //    me.sevendaysoutput = response.data["当日库存量"]
+    //    if(this.code === "yesterday"){
+    //       this.inventory = response.data["当日库存量"][0].dr_zkc;
+    //    }
+    //   });
+
+    //   this.$http.get('http://61.177.139.229:8083/Liems/webservice/getLateStockList').then((response) => {
+    //    me.sevendaysoutput = response.data["7日合计"]
+    //    if(this.code === "sevendays"){
+    //       this.output = me.sevendayinventory;
+    //    }
+    //   });
+    //   if(this.code === "yesterday"){
+    //       this.output = 2789;
+    //       this.inventory = 3131;
+    //       this.sales = 2542
+    //    }
+
+    //    if(this.code === "sevendays"){
+    //       this.output = yesterdayoutput;
+    //    }
 
         if(this.code === "month"){
           this.output = 32790;
--- src/main.js
+++ src/main.js
@@ -10,10 +10,13 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import Vant from 'vant'
 import 'vant/lib/index.css'
+import VueResource from 'vue-resource'
 
 Vue.use(Vant);
 Vue.use(ElementUI)
 Vue.use(echarts)
+Vue.use(VueResource)
+
 
 new Vue({
   render: h => h(App),
